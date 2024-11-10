57424234c723817804e3e6cc87a44619e935af74
THEBobmiester101 <mellusobenjamin@gmail.com>
2024-11-10 02:11:14 -0500

    add more game functionality and newDay function

--- game.py
+++ game.py
@@ -12,7 +12,11 @@ DEFAULT_SHOP = {
     "Magic Shop: vicious ring":             80,
     "Nothin' else":                         0
 }
-LINE_BREAK = "----------------------------------\n"
+REPEATABLE_BUYS = [
+    "A hearty meal",
+    "Gamble"
+]
+LINE_BREAK = "----------------------------------"
 GAME_OPTIONS = "(FT) Find enemy to fight\n(RR) Rest and recover\n(SM) Spend money\n(ND) Next day\n(Q) Quit"
 COMBAT_OPTIONS = ["Attack", "Dodge", "Run Away"]
 
@@ -21,7 +25,11 @@ COMBAT_OPTIONS = ["Attack", "Dodge", "Run Away"]
 class Game:
     
     player: Player
-    n_days: int = 0
+    n_days: int = 1
+    end_day_script = []
+    can_fight: bool = True
+    can_rest: bool = True
+    bought_items = []
 
 
     def __init__(self, console_line_height):
@@ -29,7 +37,8 @@ class Game:
         self.player = Player(player_name, {})
 
         for line in range(console_line_height):
-            print(self.player.catch_phrase if line == 4 else "\n")
+            print("")
+        self.end_day_script.append(f"{self.player.catch_phrase}\nDay 1 begins")
 
     
     def __del__(self):
@@ -39,52 +48,93 @@ class Game:
 
     def loop(self) -> bool:    
         is_daytime = True
+        self.newDay()
         while is_daytime:
-            player_input = input(LINE_BREAK + "What would you like to do?\n" + GAME_OPTIONS + "\n>")
+            #print(f"PLAYER HEALTH IS {self.player.stats["health"]}")
+            player_input = input("What would you like to do?\n" + GAME_OPTIONS + "\n>")
 
             if player_input == "FT":
-                enemy = self.getEnemyOptions()
-                combat_result = self.fightSequence(enemy)
-                if combat_result == "win":
-                    self.player.n_wins += 1
-                    print("You recovered " + str(enemy["gold"]) + " gold")
-                    gold_total += enemy["gold"]
-                    self.player.stats["gold"] += enemy["gold"]
-                elif combat_result == "death":
-                    if self.player.n_wins < 5:
-                        print(self.player.name + " has died. They pressed their luck.")
-                    elif self.player.n_wins < 10:
-                        print(self.player.name + " was finally struck down.")
-                    elif self.player.n_wins < 25:
-                        print(self.player.name + "\'s luck caught up with them. It took a while.")
-                    return False
-                elif combat_result == "ran":
-                    print("Made it out alive")
+                if self.can_fight:
+                    self.can_rest = False
+                    enemy = self.getEnemyOptions()
+                    combat_result = self.fightSequence(enemy)
+                    if combat_result == "win":
+                        self.player.n_wins += 1
+                        print("You recovered " + str(enemy["gold"]) + " gold")
+                        self.player.n_gold += enemy["gold"]
+                        self.player.stats["gold"] += enemy["gold"]
+                        self.end_day_script.append(f"Won a fight and got {enemy["gold"]} gold")
+                    elif combat_result == "death":
+                        if self.player.n_wins < 5:
+                            print(self.player.name + " has died. They pressed their luck.")
+                        elif self.player.n_wins < 10:
+                            print(self.player.name + "\'s luck caught up with them. It took a while.")
+                        elif self.player.n_wins < 25:
+                            print(self.player.name + " was finally struck down. They left a legacy.")
+                        return False
+                    elif combat_result == "ran":
+                        self.end_day_script.append("Ran away from a fight")
+                        print("Made it out alive")
+                else:
+                    print("You're taking it easy today, remember?")
 
             elif player_input == "RR":
-                pass
+                if self.can_rest:
+                    self.player.playerHeal()
+                    self.can_fight, self.can_rest = False, False
+                    self.end_day_script.append("Rested for most of the day")
+                    print("Took most of the day to rest and recover")
+                elif self.can_fight:
+                    print("You can't rest on a day you got in a fight!")
+                else:
+                    print("You're already taking the day to rest!")
 
             elif player_input == "SM":
-                print("What would you like to buy?")
-                for key in DEFAULT_SHOP:
-                    print(key + ": " + str(DEFAULT_SHOP[key]))
-                item_index = int(input("(Enter 1-" + str(DEFAULT_SHOP.keys().__len__()) + ")")) - 1
-                item_name = list(DEFAULT_SHOP.keys())[item_index]
-                if item_name != "Nothin' else":
-                    print(item_name)
-                    if self.player.hasGold(DEFAULT_SHOP[item_name]):
-                        self.player.stats["gold"] -= DEFAULT_SHOP[item_name]
+                shopping = True
+                while shopping:
+                    print(f"What would you like to buy? You have {self.player.stats["gold"]} gold.")
+                    for key in DEFAULT_SHOP:
+                        if key in self.bought_items:
+                            print(key + ": BOUGHT")
+                        else:
+                            print(key + ": " + str(DEFAULT_SHOP[key]))
+                    item_index = int(input("(Enter 1-" + str(DEFAULT_SHOP.keys().__len__()) + ")")) - 1
+                    item_name = list(DEFAULT_SHOP.keys())[item_index]
+                    if (item_name != "Nothin' else") and (item_name not in self.bought_items):
+                        print(item_name)
+                        if self.player.hasGold(DEFAULT_SHOP[item_name]):
+                            self.player.stats["gold"] -= DEFAULT_SHOP[item_name]
+                            self.bought_items.append(item_name)
+                            self.end_day_script.append(f"Bought {item_name}")
+                    elif item_name in self.bought_items:
+                        print("Already bought that!")
+                    else:
+                        shopping = False
 
             elif player_input == "ND":
                 is_daytime = False
-                print("Day " + str(self.n_days) + " ends")
-                self.n_days += 1
-                print("Day " + str(self.n_days) + " begins")
+                self.end_day_script.append(f"Day {self.n_days} ends")
 
             elif player_input == "Q":
                 return False
 
         return True
+
+
+    def newDay(self):
+        self.n_days += 1
+
+        for item in self.bought_items:
+            if item in REPEATABLE_BUYS:
+                self.bought_items.remove(item)
+
+        self.player.playerHeal()
+        self.can_fight, self.can_rest = True, True
+
+        # prints all statements that were added to self.end_day_script throughout the day
+        print(f"{LINE_BREAK}\n{"\n".join(self.end_day_script)}\n{LINE_BREAK}")
+        self.end_day_script.clear()
+        self.end_day_script.append(f"Day {self.n_days} begins")
 
 
     def getEnemyOptions(self):
@@ -119,8 +169,8 @@ class Game:
         turn_count = 0
 
         # debugging; prints stats of player and enemy
-        print("Player stats:\n" + str(self.player.stats.items()))
-        print("\nEnemy stats:\n" + str(enemy.items()))
+        #print("Player stats:\n" + str(self.player.stats.items()))
+        #print("\nEnemy stats:\n" + str(enemy.items()))
 
         while fighting:
             player_is_faster = (self.player.stats["dodge"] >= enemy["dodge"])
@@ -142,6 +192,7 @@ class Game:
                             attack_damage += self.player.stats["crit"]
                         damage_dealt = max(0, attack_damage - enemy["natural_armor"] - enemy["dodge"])
                         enemy["health"] -= damage_dealt
+                        print("") # adds visual gap between combat turns
                         if enemy["health"] <= 0:
                             print("A felling strike")
                         elif damage_dealt > 4:
@@ -243,6 +294,7 @@ class Game:
                 asessing_statements.append("unusually slow")
                 # not worth gold
 
+            gold = max(gold, 1) # each job is worth at least 1 gold
             enemies["gold"].append(gold)
             print("Enemy " + str(enemy + 1) + " is " + ", ".join(asessing_statements))
         choice = input("Which enemy would you like to face? Choose between " + str(enemies["number"]))
--- player.py
+++ player.py
@@ -1,9 +1,13 @@
 BASE_STATS = {
+    # combat stats
     "attack":           2,
     "crit":             1,
     "natural_armor":    1,
     "dodge":            0,
+    "max_health":       10,
     "health":           10,
+    # non-combat stats
+    "has_eaten":        False,
     "gold":             0
 }
 
@@ -20,12 +24,27 @@ class Player:
     def __init__(self, name: str, adjustment_stats: dict):
         # set name 
         self.name = name
-        
+        self.setStats(adjustment_stats, BASE_STATS)
+        #print(f"BASE STATS: {BASE_STATS}\nADJUSTMENT STATS: {adjustment_stats}")
+
+
+    def setStats(self, adjustment_stats, base_stats=None):
         # set stats
-        for stat in BASE_STATS:
-            self.stats[stat] = BASE_STATS[stat]
-            if stat in adjustment_stats:
-                self.stats[stat] += adjustment_stats[stat]
+        #print(f"base_stats = <{base_stats}>\nadjustment_stats: <{adjustment_stats}>")
+        if base_stats:
+            for stat in base_stats:
+                self.stats[stat] = base_stats[stat]
+                if stat in adjustment_stats:
+                    self.stats[stat] += adjustment_stats[stat]
+        else:
+            for stat in self.stats:
+                if stat in adjustment_stats:
+                    # checks for a stat maximum and enforces it when adding stats. E.g. the "health" stat cannot
+                    #   exceed the "max_health" stat.
+                    if ("max_" + stat) in self.stats:
+                        self.stats[stat] = min(self.stats["max_" + stat], self.stats[stat] + adjustment_stats[stat])
+                    else:
+                        self.stats[stat] += adjustment_stats[stat]
 
 
     def hasGold(self, cost: int):
@@ -35,5 +54,16 @@ class Player:
         else:
             print("You ain't got the cash")
             return False
-        
     
+
+    def playerHeal(self):
+        current_health = self.stats["health"]
+        # if player has eaten a hearty meal they restore 1/2 their hp, otherwise they restore 1/4th
+        #   both restored values are rounded up. Hence dividing by 1.9 or 3.9 respectively
+        if self.stats["has_eaten"]:
+            self.setStats({"health": round(self.stats["max_health"] / 1.9)})
+        else:
+            self.setStats({"health": round(self.stats["max_health"] / 3.9)})
+        self.stats["has_eaten"] = False
+
+        #print(f"HEALED {self.stats["health"] - current_health} HEALTH AFTER RESTING")
